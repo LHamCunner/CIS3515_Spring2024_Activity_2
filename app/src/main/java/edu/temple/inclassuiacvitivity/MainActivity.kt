aca7c242c3b81a3fc933c629a5b1c92610641f4a
Liam Cunningham <tuo20284@temple.edu>
2024-02-06 11:39:20 -0500

    Populated Array with even numbers 1-100

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -14,10 +14,15 @@ class MainActivity : AppCompatActivity() {
         val displayTextView = findViewById<TextView>(R.id.textDisplay)
 
         /* TODO Step 1: Populate this array */
-        //val numberArray = Array Of Numbers
+        var numberArray = Array<Int>(size = 50) { 0 }
+        for(n in numberArray.indices){
+            if(n % 2 == 0){
+                numberArray[n] = n;
+            }
+        }
 
         /* TODO Step 2: Create adapter to display items from array in Spinner */
-        //spinner.adapter = ArrayAdapter...
+        //spinner.adapter = ArrayAdapter
 
 
         // TODO Step 3: Change TextView's text size to the number selected in the Spinner */
